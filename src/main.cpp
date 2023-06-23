3c1116185f93dc1de7e8f3aa02b20fc1e93f5d98
Lucas Dalla Corte Franchi <lucas.franchi@dbserver.com.br>
2023-06-23 16:44:52 -0300

    squash: Initial cpp test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
+#include <iostream>
+
+class Foo {
+public:
+  Foo(int bar) { this->bar_ = bar; };
+  int getFoo(void) { return this->bar_; }
+
+private:
+  int bar_;
+};
+
 int main(void) {
-    return 0;
+  Foo f(10);
+  std::cout << f.getFoo();
+
+  return 0;
 }
 
-extern "C" void app_main(void) {
-    main();
-}
+extern "C" void app_main(void) { main(); }
