2bdad566e45c7ed3083bc16dc5f285075e58ee5b
unknown <luasd.franchi@gmail.com>
2024-07-13 23:52:02 -0300

    workflow

--- container/environment_helper.sh
+++ container/environment_helper.sh
@@ -3,7 +3,7 @@
 DOCKER_IMAGE="titanium_env"
 
 # Check if Docker image exists locally
-if [[ "$(docker images -q $DOCKER_IMAGE 2> /dev/null)" == "" ]]; then
+if [ "$(docker images -q $DOCKER_IMAGE 2> /dev/null)" == "" ]; then
   echo "Docker image '$DOCKER_IMAGE' not found. Building..."
   docker build -t $DOCKER_IMAGE .
 else
