6c38c6be222ad3e342202ef56b89cfede467243b
Lucas D. Franchi <LucasD.Franchi@gmail.com>
2024-10-07 13:10:04 -0300

    Clean up main by removing development-specific implementations
    - All the mock and injection functions used for development
      have been moved to the appropriate branch, keeping the
      main branch as clean as possible.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include "Application/Application.h"
 
-#include "CustomProcess/TimeProcess/inc/TimeProcess.h"
-
 int main(void) {
     Application app;
 
@@ -10,26 +8,6 @@ int main(void) {
     app.EnableUartProcess(10240, 5);
     app.EnableLoraProcess(20240, 5, true);
     app.EnableMQTTClientProcess(10240, 5);
-
-    app.InjectDebugCredentials("NETPARQUE_PAOLA", "NPQ196253");
-
-    app.SignUpSharedArea(MEMORY_AREAS_TIME_PROCESS, sizeof(time_process_t), READ_WRITE);
-    auto time_process = new TimeProcess("Time Proccess", 2048, 5);
-    time_process->InitializeProcess();
-
-    continuos_packet_list_t continuos_packet_list{};
-    continuos_packet_list.packet_configs_count = 1;
-    continuos_packet_list.packet_configs[0] = {
-        .destination_address = 0x0000,
-        .destination_area    = MEMORY_AREAS_TIME_PROCESS,
-        .requested_area      = MEMORY_AREAS_TIME_PROCESS,
-        .packet_interval     = 5000000,
-        .last_transmission   = 0,
-    };
-
-    app.InjectMemoryAreaData(MEMORY_AREAS_LORA_CONTINUOS_PACKET,
-                             continuos_packet_list,
-                             continuos_packet_list_t_msg);
 
     return 0;
 }
